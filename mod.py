050479a2b89308d09e667eab63433cc5813caab2
e <185975315@qq.com>
2019-02-13 22:23:54 +0800

    python 高阶

--- mod.py
+++ mod.py
@@ -23,3 +23,49 @@ tree=ET.parse('data.xml');
 print(tree)#tag标签名，attrib属性值同js得attribute，text取标签间得文本
 s='avavavaalexdvavadvad'
 print(s.find('alex'))
+import re
+res=re.findall('\d+','vadkvabouvy348914y1903fhvajkdv12312dvadvad');
+print(res)
+print(re.findall('alex','aelexakdvjbadiovbad'))
+print(re.findall("a..x",'vakjvazvjadjkbvxaddx'))
+print(re.findall('^a..x','assxadvxadv'))
+print(re.findall('a..x','assxadvxadv'))
+print(re.findall('a..x$','assxadvxadavvx'))
+print(re.findall('a..x\$$','assxadvxadavvx$'))
+print(re.findall('d*','assxadvxadavvx$dd'))
+print(re.findall('d+','assxadvxadavvx$dd'))
+print(re.findall('alex+','alvalexxxxvakdjbv'))
+print(re.findall('alex*','alvalevakdjbv'))
+print(re.findall('alex+','alvalevakdjbv'))
+print(re.findall('alex?','alvalexxvakdjbv'))
+print(re.findall('alex?','alvalevakdjbv'))
+print(re.findall('alex{6}','alvalexxxxxxxxvakdjbv'))
+print(re.findall('alex+?','alvalexxxxxxxxvakdjbv'))
+"""
+    ^表示字符开头，
+    $表示字符结尾，
+    *表示0到无穷个，
+    +表示1到无穷个，
+    ？表示0，1次,
+    一个.只匹配一个字符
+    加上？变成惰性匹配，按最小匹配
+    {}  ==> {0,}==*
+        {1,}==+
+        {0,1}==?
+        可表示所有
+    []表示里面的内容是或,[]里的^表示非，[^0-9]表示非0-9的字符，-表示范围，\表示转义
+    \d表示[0,9]
+    \D表示[^0-9]
+    \s匹配任何空白字符==[\t\n\r\f\v]
+    \S==[^\t\n\r\f\v]
+    \w==[a-zA-Z0-9_]
+    \W==[^a-zA-Z0-9_]
+    \b匹配特殊字符边界，如空格，#，&等
+"""
+print(re.findall("www[oldboy baidu]",'wwwbaidu'))
+print(re.findall('x[yz]p','xypzzxczxzpczcz'))
+print(re.findall('q[a-z]','qf'))
+print(re.findall('q[^a-z]+','q12f'))
+#12+(34*6+2-5*(2-1)), 匹配2-1
+print(re.findall("\([^()]+\)","12+(34*6+2-5*(2-1))"))
+print(re.findall("\d+","12+(34*6+2-5*(2-1))"))
