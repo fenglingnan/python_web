b557f1b32fd8ed295e9d3540eafb2c8e39d69c82
Jhin <wuqijing@zhijiesong.com>
2019-02-15 10:18:29 +0800

    pycharm上传

--- mod.py
+++ mod.py
@@ -61,6 +61,7 @@ print(re.findall('alex+?','alvalexxxxxxxxvakdjbv'))
     \w==[a-zA-Z0-9_]
     \W==[^a-zA-Z0-9_]
     \b匹配特殊字符边界，如空格，#，&等
+    ()表示整体
 """
 print(re.findall("www[oldboy baidu]",'wwwbaidu'))
 print(re.findall('x[yz]p','xypzzxczxzpczcz'))
@@ -69,3 +70,32 @@ print(re.findall('q[^a-z]+','q12f'))
 #12+(34*6+2-5*(2-1)), 匹配2-1
 print(re.findall("\([^()]+\)","12+(34*6+2-5*(2-1))"))
 print(re.findall("\d+","12+(34*6+2-5*(2-1))"))
+print(re.findall('I\\b','hello,I am CHINESE'))
+print(re.findall(r'I\b','hello,I am CHINESE'))
+print(re.findall(r'c\\l','abc\lqeg'))
+print(re.findall('c\\\\l','abc\lqeg'))#两层转义，re的转义和python解释器的转义
+print(re.findall('k[a\|b]','ka|kbc\lqeg'))
+print(re.findall('(abc)+','abcccc'))
+print(re.findall('(?:abc)+','abcabcabc'))
+print(re.findall('(?P<name>\w+)','abcccc'))
+print(re.search('(?P<name>\w+)','abcccc'))#search只找一个就停止
+print(re.search('(?P<name>\w+)','abcccc').group())
+print(re.search('(?P<name>[a-z]+)\d+','alex34cx22jx23').group())
+print(re.search('(?P<name>[a-z]+)\d+','alex34cx22jx23').group('name'))
+print(re.search('(?P<name>[a-z]+)(?P<age>\d+)','alex34cx22jx23').group('age'))
+print(re.match('\d+','ad23'))#math只匹配开始位置，search替代
+print(re.match('\d+','23ad23'))
+print(re.split('[ |]','hello world|lin'))#split不是一次性全分割完，匹配成功分割一次
+print(re.split('[ab]','asdabcd'))
+print(re.sub('\d','A','222avaodvhad'))#替换，数字参数是次数
+print(re.sub('\d','A','222avaodvhad',2))
+print(re.subn('\d','A','222avaodvhad5'))
+com=re.compile('\d+')#同js的reg
+print(com.findall('198hf198eh'))
+ne=re.finditer('\d+','09uva209duva09dv09')#findall迭代器版本
+print(ne)
+print(next(ne).group())
+print(next(ne).group())
+print(ne.__next__().group())
+print(re.findall('http://www\.(baidu|163)\.com','vadvadvhttp://www.baidu.comvdadvad'))#优先匹配分组里的内容
+print(re.findall('http://www\.(?:baidu|163)\.com','vadvadvhttp://www.baidu.comvdadvad'))#?:去除优先级
