d58b6b33fdd3d846c3019edae9d6b71b9cb58885
e <185975315@qq.com>
2019-05-15 00:28:32 +0800

    python 升级

--- work.py
+++ work.py
@@ -27,4 +27,15 @@ class Course(Base):
 # school_obj=pickle.load(open('work.db','rb'))
 # print(school_obj,'-'*10,school_obj.name,school_obj.addr)
 s1=School('oldboy','北京')
-s1.save()
+# s1.save()
+
+class Hello(object):
+    _str='111'
+    #单下划线开头表示私有，不应该在外部使用，python不会真的阻止调用，import里不会出现
+    __star='earth'
+    # 双下划线python会重新命名，_类名__属性名,无法直接访问
+    stu='222'
+
+h1=Hello();
+print(h1.stu)
+print(Hello.stu)
