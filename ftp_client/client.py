b59b9579247dc9279f1d088edf3e2407fa46cabd
fenglingnan <185975315@qq.com>
2019-11-18 23:50:16 +0800

    需要加启动命令，所以需要在命令行打开

--- ftp_client/client.py
+++ ftp_client/client.py
@@ -1,5 +1,71 @@
-
+import optparse
 import socket
+import configparser
+import json
 
-sk=socket.socket()
-sk.connect(('127.0.0.1',8080))
+class ClientHandler():
+
+    def __init__(self):
+
+        self.op=optparse.OptionParser()
+        self.op.add_option('-s','--server',dest='server')
+        self.op.add_option('-P', '--port', dest='port')
+        self.op.add_option('-u', '--username', dest='username')
+        self.op.add_option('-p', '--password', dest='password')
+
+        self.opt,self.args=self.op.parse_args()
+        self.verify_args(self.opt,self.args)
+        self.run_connect()
+
+    def verify_args(self,opt,args):
+
+        server=opt.server
+        port=opt.port
+        # username=opt.username
+        # password=opt.password
+
+        if int(port)>0 and int(port)<65535:
+            return True
+
+        else:
+            exit('port is in 0-65535')
+
+    def run_connect(self):
+
+        self.sock=socket.socket()
+        print(self.opt)
+        self.sock.connect((self.opt.server,int(self.opt.port)))
+
+    def interactive(self):
+
+        self.authenticate()
+
+    def authenticate(self):
+
+        if self.opt.username is None or self.opt.password is None:
+
+            username=input('username:')
+            password=input('password:')
+            return self.get_auth(username,password)
+        return self.get_auth(self.opt.username,self.opt.password)
+
+    def get_data(self):
+
+        data = self.sock.recv(1024).decode('utf-8')
+        data = json.loads(data)
+        return data
+
+    def get_auth(self,user,pwd):
+
+        data={
+            'action':'auth',
+            'username':user,
+            'password':pwd
+        }
+        self.sock.send(json.dumps(data).encode('utf-8'))
+        res=self.get_data()
+        print(res)
+
+
+ch=ClientHandler()
+ch.interactive()
--- ftp_server/core/server.py
+++ ftp_server/core/server.py
@@ -1,8 +1,26 @@
 
-
+import json
 import socketserver
 
 class ServerHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
-        print('ok')
+
+        while True:
+            #conn=self.request
+            data=self.request.recv(1024).strip()
+            data=json.loads(data.decode('utf-8'))
+            if data.get('action'):
+                if hasattr(self,data.get('action')):
+                    func=getattr(self,data.get('action'))
+                    func(**data)
+                else:
+                    print('invalid cmd')
+            else:
+                print('invalid cmd')
+    def auth(self,**data):
+
+        print('data is',data)
+
+    def pu(self,**data):
+        pass
